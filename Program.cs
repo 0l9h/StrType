cfeba29b784c6ff619cf991413aaf5bd4fdee0e9
Oleh <olehbobeliak@gmail.com>
2021-07-10 15:18:38 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StrType
+{
+    class MyString
+    {
+        List<char> _data = new List<char>();
+
+        public MyString
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
